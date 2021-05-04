4ccb50a90b582ea514d377300d1cd095242f6b18
SaraEmiliaBal <sara_emilia01@outlook.com>
2021-05-04 16:06:39 -0400

    creation deuxieme page d'archive

---
+++ theme4w4/js/rest_api.js
@@ -0,0 +1,24 @@
+(function(){
+
+	function monAjax()
+{
+   let maRequete = new XMLHttpRequest();
+   console.log(maRequete)
+   maRequete.open('GET', 'http://localhost:8888/4w4-3/wp-json/wp/v2/posts');
+   maRequete.onload = function () {
+       console.log(maRequete)
+       if (maRequete.status >= 200 && maRequete.status < 400) {
+           let data = JSON.parse(maRequete.responseText);
+        }
+       nouvelle.innerHTML = maChaine;
+       } else {
+           console.log('La connexion est faite mais il y a une erreur')
+       }
+   }
+   maRequete.onerror = function () {
+       console.log("erreur de connexion");
+   }
+   maRequete.send()
+}
+
+}())
